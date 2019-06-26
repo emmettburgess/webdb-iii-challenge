749a1685b6e31f0bebcf38a4d2382d6bd860ff19
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-06-26 14:37:21 -0600

    exports

--- migrations/20190626143513_create_cohorts_table.js
+++ migrations/20190626143513_create_cohorts_table.js
@@ -1,8 +1,13 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('cohorts', function(tbl) {
+      tbl.increments();
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique();
+    });
+  };
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('cohorts');
+  };
--- migrations/20190626143537_create_students_table.js
+++ migrations/20190626143537_create_students_table.js
@@ -1,8 +1,18 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('students', function(tbl) {
+      tbl.increments(); name, 128, unique, not null
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      tbl
+        .integer('cohort_id')
+        .unsigned()
+        .references('id')
+        .inTable('cohorts')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    });
+  };
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex, Promise) {};
